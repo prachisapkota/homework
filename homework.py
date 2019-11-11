facde11f87ccb0354603c0b8bce33af4acecf3ee
prachi <prachisapkota21@gmail.com>
2019-11-11 07:06:54 -0800

    hy

---
+++ homework.py
@@ -0,0 +1,9 @@
+input("name")
+int(input("age"))
+x=int(input("first number"))
+y=int(input("second number"))
+sum=x+y
+sub=x-y
+product=x*y
+div=(x/y)
+print("sum=%d\nsub=%d\nproduct=%d\ndiv=%.2f"%(sum,sub,product,div))
