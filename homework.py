433a2627c8726fbe314f59047a04d9f6932eb2c7
prachi <prachisapkota21@gmail.com>
2019-11-12 05:54:03 -0800

    hey

--- homework.py
+++ homework.py
@@ -1,6 +1,9 @@
-list=[2,4,5,7,9]
-print(max(list))
-print(min(list))
+string=input("enter any string u like")
+word=input("which one would u like to replace?")
+word1=input("what would u like to replace to?")
+new=(string.replace(word,word1))
+print(new)
+print(new.upper())
 
 
 
