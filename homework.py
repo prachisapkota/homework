04b35d09934755b68c45d770ea9fa5ec1ef0a77d
prachi <prachisapkota21@gmail.com>
2019-11-11 22:34:06 -0800

    school

--- homework.py
+++ homework.py
@@ -1,36 +1,6 @@
-list=[1,2,4,5,6]
-tuples=(1,3,4,5,7)
-print(list[2])
-print(tuples[1])
-
-list=["prachi"]
-list.append(2)
-print(list)
-
-list=[1,4,6,7]
-list.pop(3)
-print(list)
-
-list=[9,8,7,6,5]
-list.sort()
-print(list)
-
-list1=[1,2,3,4,5,6]
-list1.sort(reverse=True)
-print(list1)
-list=[1,1,1,3,4,5,5,6]
-print(list.count(1))
-
-list=[1,2,3,4]
-list1=[5,6,7,8]
-list.extend(list1)
-print(list)
-
-name= ("prachi sapkota")
-print(name[1:6])
-
-list=[1,2,3,4,5,6,7,8,9,10,11,12]
-print(list[1:10:2])
+list=[2,4,5,7,9]
+print(max(list))
+print(min(list))
 
 
 
