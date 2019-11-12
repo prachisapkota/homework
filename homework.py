1450f12018f915f2a557823f87075a1482202dab
prachi <prachisapkota21@gmail.com>
2019-11-12 09:58:47 -0800

    yo

--- homework.py
+++ homework.py
@@ -1,9 +1,61 @@
-string=input("enter any string u like")
-word=input("which one would u like to replace?")
-word1=input("what would u like to replace to?")
-new=(string.replace(word,word1))
-print(new)
-print(new.upper())
+'''money=int(input("how much money do u have?"))
+if money >50:
+    print("you can go to party")
+else:
+    print("you cannot go to party")
+
+marks=int(input("enter your marks"))
+if marks >40:
+    print("you are pass")
+else:
+    print("you are fail")'''
+
+'''age=int(input("enter your age"))
+if age>18:
+    print("you can drive")
+else:
+    print("you cannot drive")'''
+
+'''marks=int(input("enter your marks"))
+if marks >=90 and marks <=100:
+    print("you got A")
+elif marks >=70 and marks <90:
+    print("you got B")
+elif marks >=40 and marks <70:
+    print("you got C")
+elif marks >=0 and marks <40:
+    print("you are fail")'''
+
+'''value=int(input("enter a value"))
+if value==0:
+    print("it is zero")
+elif value%2==0:
+    print("it is even")
+elif num<0:
+    print("it is negative")
+else:
+     print("odd")'''
+
+'''year=int(input("enter the year"))
+if year%4==0 and year % 100 !=0:
+    print("year is a leap year")
+else:
+    print("year is not a leap year")'''
+
+name=str(input("enter your name"))
+age=int(input("enter your age"))
+if age >18:
+    print("it is valid")
+else:
+    print("it is not valid")
+number=int(input("enter your mobile number"))
+
+
+
+
+
+
+
 
 
 
