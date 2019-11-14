6d32c18f05e92699672cdc5bfced6a0bdca0dc68
prachi <prachisapkota21@gmail.com>
2019-11-13 20:28:24 -0800

    add

--- homework.py
+++ homework.py
@@ -1,81 +1,24 @@
-'''money=int(input("how much money do u have?"))
-if money >50:
-    print("you can go to party")
-else:
-    print("you cannot go to party")
+'''list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+even_list=[]
+for x in list:
+    if x%2==0:
+        even_list.append(x)
+print("The list of even numbers is")
+print(even_list)'''
 
-marks=int(input("enter your marks"))
-if marks >40:
-    print("you are pass")
-else:
-    print("you are fail")'''
+'''number=int(input("enter any number"))
+print("the multiplication table of %s :" % number)
+for x in range(1,11):
+    print("%s = %s =%s" %(number, x, number*x))'''
 
-'''age=int(input("enter your age"))
-if age>18:
-    print("you can drive")
-else:
-    print("you cannot drive")'''
+'''list=[1,2,3,4,5,6,7,8,9]
+number=int(input("enter the number you want to remove"))
+list.pop(number)
+print(list)'''
 
-'''marks=int(input("enter your marks"))
-if marks >=90 and marks <=100:
-    print("you got A")
-elif marks >=70 and marks <90:
-    print("you got B")
-elif marks >=40 and marks <70:
-    print("you got C")
-elif marks >=0 and marks <40:
-    print("you are fail")
-else:
-    print("sorry, wrong input number")
-    
-
-value=int(input("enter a value"))
-if value==0:
-    print("it is zero")
-elif value%2==0:
-    print("it is even")
-elif num<0:
-    print("it is negative")
-else:
-     print("odd")'''
-
-'''year=int(input("enter the year"))
-if year%4==0 and year % 100 !=0:
-    print("year is a leap year")
-else:
-    print("year is not a leap year")'''
-
-'''name=str(input("enter your name"))
-if name.isalpha()==True and len(name)>3:
-    print("valid name")
-else:
-    sys.exit("OOPS! Invalid name")
-phonenum=input("enter you phnnumber")
-if phonenum.isdigit()==True and len (phonenum)==10:
-    print("valid phone number")
-else:
-    sys.exit("OOPS!Invalid phone num")
-age=input("enter your age")
-if age.isdigit()==True and int(age)>=18:
-    print("it is valid")
-else:
-    print("it is not valid")'''
-
-'''length1=int(input("enter the first side:"))
-length2=int(input("enter the second side:"))
-length3=int(input("enter the third side:"))
-length4=int(input("enter the fourth side:"))
-if length1==length2==length3==length4:
-    print("it is a square")
-elif length2==length1 and length3==length4 and length1==length3 and length2==length4:
-    print("it is a rectangle")
-elif length3==length1 and length2==length4 and length1==length4 and length2==length3:
-    print("it is a kite")
-else:
-    print("it is a quadrilateral")'''
-
-
-
+numbers=input("enter the numbers")
+for numbers in range(1,11):
+    print [list(numbers )]
 
 
 
